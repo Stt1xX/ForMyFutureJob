b2b56869414bb15b080394dd274098080c358dc1
Timur <stt1xx@mail.ru>
2023-04-20 00:18:18 +0300

    execute_script has defeated

--- Second sem/Programming/Lab5/Work/progalab5/src/main/java/fergie/me/Commands/ExecuteScript.java
+++ Second sem/Programming/Lab5/Work/progalab5/src/main/java/fergie/me/Commands/ExecuteScript.java
@@ -2,30 +2,30 @@ package fergie.me.Commands;
 
 import fergie.me.CollectionManager;
 import fergie.me.CommandManager;
-import fergie.me.FileManager;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ExecuteScript extends InputCommand implements Command {
 
-    int counter; //для рекурсии
+    private static int counter; //для рекурсии
     String description = "execute_script file_name: считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.";
 
     public ExecuteScript(CollectionManager collectionManager, Scanner scanner) {
         super(collectionManager, scanner);
     }
-    public void execute() {
+    public void execute(){
         if (counter < 20) {
             System.out.println("Введите путь до скрипта (включая его имя):");
+            counter += 1;
             Path path = Paths.get(scanner.nextLine());
             try {
                 Scanner scannerForScript = new Scanner(path);
                 String arg[];
                 CommandManager commandManagerForScript = new CommandManager(collectionManager, scannerForScript);
-                counter += 1;
                 while (scannerForScript.hasNextLine()) {
                     String s = (scannerForScript.nextLine());
                     arg = s.split("\s+");
@@ -42,13 +42,17 @@ public class ExecuteScript extends InputCommand implements Command {
                         System.out.println("Слишком длинная команда.");
 
                 }
-                System.out.println("Скрипт выполнен");
+                System.out.println("Скрипт выполнен.");
             } catch (IOException e) {
-                System.out.println("Ошибка в пути к файлу");
+                System.out.println("Ошибка в пути к файлу.");
+            } catch (NoSuchElementException e){
+                System.out.println("Скрипт некорректный, переделывай!");
+            } finally {
+                counter -= 1;
             }
         }
         else{
-            System.out.println("Блокировка скрипта, из-за рекурсии");
+            System.out.println("Блокировка скрипта, из-за рекурсии.");
         }
     }
 
