818151f786e6655b7bae4c20a72be9a8f10f6790
Timur <stt1xx@mail.ru>
2023-05-05 17:20:03 +0300

    I'm a hacker

--- Second sem/Programming/Lab5/Work/progalab5/src/main/java/fergie/Commands/ExecuteScript.java
+++ Second sem/Programming/Lab5/Work/progalab5/src/main/java/fergie/Commands/ExecuteScript.java
@@ -7,16 +7,14 @@ import java.io.IOException;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
 public class ExecuteScript extends InputCommand implements Command {
 
     private static int counter; //для рекурсии
     private static int mode;
 
-    private static ArrayList<String> arrayList = new ArrayList<>();
+    private static Stack<String> stackOfScripts = new Stack<>();
     String description = "execute_script file_name: считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.";
 
     public static int getMode(){
@@ -31,20 +29,19 @@ public class ExecuteScript extends InputCommand implements Command {
     public void execute(String str){
         setMode(2);
 
-        if(arrayList.contains(str)){
-            System.out.println("Скрипт завершается досрочно из-за рекурсии. ");
-            return;
+        //проверка рекурсии
+        stackOfScripts.push(str);
+        if(Collections.frequency(stackOfScripts, str) == 2){
+            System.out.println("Продолжение выполнения скрипта невозможно. Удостоверьтесь, что рекурсия в скрипте отсутствует.");
+            System.exit(0);
         }
-        else{
-            arrayList.add(str);
-        }
-        if (counter < 20) {
-            counter += 1;
-            Path path = Paths.get(str);
-            try {
-                Scanner scannerForScript = new Scanner(path);
-                String arg[];
-                CommandManager commandManagerForScript = new CommandManager(collectionManager, scannerForScript);
+
+
+        Path path = Paths.get(str);
+        try {
+            Scanner scannerForScript = new Scanner(path);
+            String arg[];
+            CommandManager commandManagerForScript = new CommandManager(collectionManager, scannerForScript);
 //                while (scannerForScript.hasNextLine()) {
 //                    String s = (scannerForScript.nextLine());
 //                    arg = s.split("\s+");
@@ -64,46 +61,42 @@ public class ExecuteScript extends InputCommand implements Command {
 //                        System.out.println("Слишком длинная команда.");
 //
 //                }
-                while (scannerForScript.hasNextLine()) {
+            while (scannerForScript.hasNextLine()) {
 
-                    String s = (scannerForScript.nextLine());
-                    arg = s.split("\s+");
-                    try{
+                String s = (scannerForScript.nextLine());
+                arg = s.split("\s+");
+                try{
 //                    if (arg.length == 2)
 ////                        commandManager.getCommands().get(arg[0].toLowerCase()).execute(arg[1] + ' ' + arg[2]);
 ////                    else
 ////                        commandManager.getCommands().get(s.toLowerCase()).execute("");
-                        StringBuilder strForScript = new StringBuilder();
-                        for (String value : arg) {
-                            strForScript.append(value).append(" ");
-                        }
-                        if (arg.length > 1)
-                            commandManagerForScript.getCommands().get(arg[0].toLowerCase()).execute(strForScript.
-                                    substring(arg[0].length() + 1, strForScript.length() - 1));
-                        else
-                            commandManagerForScript.getCommands().get(arg[0].toLowerCase()).execute("");
-                    } catch (NullPointerException e) {
-                        System.out.println("Указана несуществующая команда (возможно, в скрипте ошибка).");
-                        return;
-                    } catch (InvalidPathException e){
-                        System.out.println("Путь к файлу не должен содержать следующих знаков: * ? < > |");
-                    } catch (IllegalArgumentException e){
-                        System.out.println("Данная команда не имеет аргументов.");
+                    StringBuilder strForScript = new StringBuilder();
+                    for (String value : arg) {
+                        strForScript.append(value).append(" ");
                     }
+                    if (arg.length > 1)
+                        commandManagerForScript.getCommands().get(arg[0].toLowerCase()).execute(strForScript.
+                                substring(arg[0].length() + 1, strForScript.length() - 1));
+                    else
+                        commandManagerForScript.getCommands().get(arg[0].toLowerCase()).execute("");
+                } catch (NullPointerException e) {
+                    System.out.println("Указана несуществующая команда (возможно, в скрипте ошибка).");
+                    return;
+                } catch (InvalidPathException e){
+                    System.out.println("Путь к файлу не должен содержать следующих знаков: * ? < > |");
+                } catch (IllegalArgumentException e){
+                    System.out.println("Данная команда не имеет аргументов.");
+                }
 //            } else
 //                System.out.println("Слишком длинная команда.");
-                }
-                System.out.println("Скрипт выполнен.");
-            } catch (IOException e) {
-                System.out.println("Ошибка в пути к файлу.");
-            } catch (NoSuchElementException e){
-                System.out.println("Скрипт некорректный, переделывай!");
-            } finally {
-                counter -= 1;
             }
-        }
-        else{
-            System.out.println("Блокировка скрипта из-за рекурсии.");
+            System.out.println("Скрипт выполнен.");
+        } catch (IOException e) {
+            System.out.println("Ошибка в пути к файлу.");
+        } catch (NoSuchElementException e){
+            System.out.println("Скрипт некорректный, переделывай!");
+        } finally {
+            stackOfScripts.pop();
         }
     }
 
